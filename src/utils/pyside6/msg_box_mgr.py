ee5df2cccd3a5ac6c89cd95a52a9040fc631bb9d
Pation.Tsai <Paction.Cai@Gmail.com>
2024-10-05 14:44:57 +0800

    Improve the implementation and structure of QMessageBox manager.

--- src/utils/pyside6/msg_box_mgr.py
+++ src/utils/pyside6/msg_box_mgr.py
@@ -3,36 +3,47 @@
 """
 Copyright©2024 Xiamen Tianma Display Technology Co., Ltd. All rights reserved.
 """
-import logging
+from logging import Logger
 
 from PySide6 import QtWidgets
 from PySide6.QtCore import Signal
 from PySide6.QtWidgets import QMessageBox, QStyle
 
-
-class CustomMessageBox(QMessageBox):
+class ManagedMsgBox(QMessageBox):
     # Define a custom signal with the QMessageBox instance and the result
     finished_with_box = Signal(QMessageBox, int)
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, parent = None, logger: Logger=None, *args, **kwargs):
+        super().__init__(parent, *args, **kwargs)
+        self._logger = logger
 
     def done(self, result):
         # Emit the custom signal with the QMessageBox instance and the result
+        if self._logger:
+            self._logger.info("ManagedMessageBox done called.")
         self.finished_with_box.emit(self, result)
         super().done(result)
 
+    def closeEvent(self, event):
+        """
+        Avoid the problem of causing the main process to exit when
+            closing the message box displayed using the show method
+            through the close button in the upper right corner of the window.
+        :param event: Contains information about the close event
+        """
+        if self._logger:
+            self._logger.info("ManagedMessageBox closeEvent called.")
+        super().closeEvent(event)  # 确保调用父类的 closeEvent
 
-class QtMsgBox:
+
+class MsgBoxMgr:
     DEFAULT_MAX_MSG_BOX_NUM = 8
 
-    def __init__(self, max_box_num: int = None, logger: logging.Logger = None):
-        if max_box_num is None:
-            self._max_box_num = QtMsgBox.DEFAULT_MAX_MSG_BOX_NUM
-        else:
-            self._max_box_num = max_box_num
+    def __init__(self, parent = None, logger=None, max_box_num: int = None):
+        self._parent = parent
+        self._logger = logger
+        self._max_box_num = max_box_num or MsgBoxMgr.DEFAULT_MAX_MSG_BOX_NUM
         self._msg_box_list = []
-        self._logger = logger or logging.getLogger(__name__)
 
     @staticmethod
     def fill_msg_box(msg_box: QMessageBox, title: str, err_msg: str,
@@ -53,20 +64,21 @@ class QtMsgBox:
         # Create an object of message box
         msg_box = QMessageBox()
 
-        QtMsgBox.fill_msg_box(msg_box, title, err_msg, QStyle.StandardPixmap.SP_MessageBoxInformation, box_icon)
+        MsgBoxMgr.fill_msg_box(msg_box, title, err_msg, QStyle.StandardPixmap.SP_MessageBoxInformation, box_icon)
 
         # Execute the message box
         msg_box.exec()
 
     def show(self, title: str, err_msg: str, box_icon: QMessageBox.Icon) -> None:
         if len(self._msg_box_list) >= self._max_box_num:
-            self._logger.warning("Too many message box, discard new one, error message: %s.", err_msg)
+            if self._logger:
+                self._logger.warning("Too many message box, discard new one, error message: %s.", err_msg)
             return
 
         # Create an object of custom message box
-        msg_box = CustomMessageBox()
+        msg_box = ManagedMsgBox(self._parent)
 
-        QtMsgBox.fill_msg_box(msg_box, title, err_msg, QStyle.StandardPixmap.SP_MessageBoxInformation, box_icon)
+        MsgBoxMgr.fill_msg_box(msg_box, title, err_msg, QStyle.StandardPixmap.SP_MessageBoxInformation, box_icon)
 
         # Set the handler for box close event
         msg_box.finished_with_box.connect(self.on_message_box_closed)
@@ -79,7 +91,9 @@ class QtMsgBox:
         msg_box.activateWindow()
 
     def on_message_box_closed(self, msg_box, result):
-        self._logger.info("Received close event, instance: %s, result: %d.", msg_box, result)
+        if self._logger:
+            self._logger.info("Received close event, instance: %s, result: %d.", msg_box, result)
         if msg_box in self._msg_box_list:
             self._msg_box_list.remove(msg_box)
-            self._logger.info("Removed close event from list success.")
+            if self._logger:
+                self._logger.info("Removed close event from list success.")
